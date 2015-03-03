b1fad77d50b5c7f1a5f601ba3efc4ce5de5325a0
Semir <semko2m@gmail.com>
2015-03-03 13:00:12 +0100

    first comit

---
+++ settings.php
@@ -0,0 +1,8 @@
+<?php
+	$adresa="244 W Roosevelt Rd Lombard IL 60148"; //jako je vazno navesti dobro adresu radi boljeg poziciniranja
+	$grad="";//jako je vazno ako adresa ima dva imena povezati ih sa plus
+	$skracenica=""; // AKo ne znate skracenicu ostavite praznu
+	$telephone=6304953623;// Broj koji se poziva na dugme CALL US
+	$theme="b";//Thema koja se aplicira na sve
+	$transitionefect="flip"; //kucate tranziciju koju zelite na stranama . Vrste tranzicija : "fade", "flip", "pop", "slide", "slidedown", "slideup"
+?>
